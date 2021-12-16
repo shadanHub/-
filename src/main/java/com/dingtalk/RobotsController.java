1817a9a7fbdd1b8c574f8a26828d2edf6f539d96
benzhao <zhao.ben@cesgroup.com.cn>
2021-12-16 16:58:04 +0800

    commit

--- src/main/java/com/dingtalk/RobotsController.java
+++ src/main/java/com/dingtalk/RobotsController.java
@@ -4,12 +4,16 @@ import com.alibaba.fastjson.JSONObject;
 import com.dingtalk.api.DefaultDingTalkClient;
 import com.dingtalk.api.DingTalkClient;
 import com.dingtalk.api.request.OapiImChatScencegroupMessageSendV2Request;
+import com.dingtalk.api.request.OapiRobotSendRequest;
 import com.dingtalk.api.response.OapiImChatScencegroupMessageSendV2Response;
+import com.dingtalk.api.response.OapiRobotSendResponse;
 import com.dingtalk.constant.AppConstant;
 import com.dingtalk.constant.UrlConstant;
 import com.dingtalk.util.AccessTokenUtil;
 import com.taobao.api.ApiException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 /**
  * 实现了机器人的简单问答功能
@@ -18,29 +22,78 @@ import org.springframework.web.bind.annotation.*;
 public class RobotsController {
 
     private static final String cardMSG = "测试卡片消息";
+    private static final String Hello = "你好";
+    private static final String DrawLots = "抽签";
 
-    @RequestMapping(value = "/robots")
-    public String helloRobots(@RequestBody(required = false) JSONObject json
-    ) {
+
+
+
+    @RequestMapping(value = "/robots", method = RequestMethod.POST)
+    public String helloRobots(@RequestBody(required = false) JSONObject json) {
         System.out.println(json);
         String content = json.getJSONObject("text").get("content").toString().replaceAll(" ", "");
-        if (cardMSG.contains(content)) {
-            return actionCardMsg();
+        // 获取用户手机号，用于发送@消息
+        // String mobile = getUserMobile(json.getString("senderStaffId"));
+        String sessionWebhook = json.getString("sessionWebhook");
+        DingTalkClient client = new DefaultDingTalkClient(sessionWebhook);
+
+        if (Hello.contains(content)) {
+            hello(client);
+        } else if(DrawLots.contains(content)){
+            drawLots(client);
+        }else {
+            learning(client);
         }
-        return learning();
+        return null;
+    }
+    /**
+     * 抽签
+     */
+    private void drawLots(DingTalkClient client) {
+        try {
+            OapiRobotSendRequest request = new OapiRobotSendRequest();
+            request.setMsgtype("text");
+            OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+            //随机抽签
+            String[] branches = {"斯莱特林","格兰芬多","拉文克劳","赫奇帕奇"};
+            Random r = new Random(System.currentTimeMillis());
+            int  i = r.nextInt(branches.length);
+            text.setContent(branches[i]);
+            request.setText(text);
+            // OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
+            // at.setAtMobiles(Arrays.asList(mobile));
+            // isAtAll类型如果不为Boolean，请升级至最新SDK
+            // at.setIsAtAll(false);
+            // request.setAt(at);
+            OapiRobotSendResponse response = client.execute(request);
+            System.out.println(response.getBody());
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
     }
 
-    /**
-     * 直接回复文本消息
-     */
-    private String learning() {
 
-        return "{" +
-            "    \"text\": {" +
-            "        \"content\": \"我就是我, 是不一样的烟火\"" +
-            "    }," +
-            "    \"msgtype\": \"text\"" +
-            "}";
+
+    /**
+     * 回答其他
+     */
+    private void learning(DingTalkClient client) {
+        try {
+            OapiRobotSendRequest request = new OapiRobotSendRequest();
+            request.setMsgtype("text");
+            OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+            text.setContent("我是问好机器人 ~");
+            request.setText(text);
+            // OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
+            // at.setAtMobiles(Arrays.asList(mobile));
+            // isAtAll类型如果不为Boolean，请升级至最新SDK
+            // at.setIsAtAll(false);
+            // request.setAt(at);
+            OapiRobotSendResponse response = client.execute(request);
+            System.out.println(response.getBody());
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -69,6 +122,30 @@ public class RobotsController {
         } catch (ApiException e) {
             e.printStackTrace();
             throw new RuntimeException();
+        }
+    }
+
+
+
+    /**
+     * 回答你好
+     */
+    public void hello(DingTalkClient client) {
+        try {
+            OapiRobotSendRequest request = new OapiRobotSendRequest();
+            request.setMsgtype("text");
+            OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+            text.setContent("你也好 ~");
+            request.setText(text);
+            // OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
+            // at.setAtMobiles(Arrays.asList(mobile));
+            // isAtAll类型如果不为Boolean，请升级至最新SDK
+            // at.setIsAtAll(false);
+            // request.setAt(at);
+            OapiRobotSendResponse response = client.execute(request);
+            System.out.println(response.getBody());
+        } catch (ApiException e) {
+            e.printStackTrace();
         }
     }
 }
